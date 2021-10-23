d3a009a577ad22396f9562fdd8cbde2bbd2afa17
kev-ae <kevin.nguyenelhs@gmail.com>
2021-10-23 03:00:05 -0400

    added the load balancer

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_launch_configuration" "instance" {
   image_id        = "ami-0c55b159cbfafe1f0"
   instance_type   = "t2.micro"
-  security_groups = ["aws_security_group.instance.id"]
+  security_groups = [aws_security_group.instance.id]
 
   user_data = <<-EOF
     #!/bin/bash
@@ -24,7 +24,7 @@ resource "aws_security_group" "instance" {
   ingress {
     from_port   = var.server_port
     to_port     = var.server_port
-    protocol    = tcp
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
@@ -32,6 +32,9 @@ resource "aws_security_group" "instance" {
 resource "aws_autoscaling_group" "example" {
   launch_configuration = aws_launch_configuration.instance.name
   vpc_zone_identifier  = data.aws_subnet_ids.default.ids
+
+  target_group_arns = [aws_lb_target_group.asg.arn]
+  health_check_type = "ELB"
 
   max_size = 10
   min_size = 2
@@ -42,10 +45,89 @@ resource "aws_autoscaling_group" "example" {
     propagate_at_launch = true
   }
 }
+
+resource "aws_lb" "loadbalancer" {
+  name               = "my-load-balancer"
+  load_balancer_type = "application"
+  subnets            = data.aws_subnet_ids.default.ids
+  security_groups    = [aws_security_group.alb.id]
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.loadbalancer.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: Page Not Found"
+      status_code  = 404
+    }
+  }
+}
+
+resource "aws_security_group" "alb" {
+  name = "terraform-example-alb"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb_target_group" "asg" {
+  name     = "terraform-asg-example"
+  port     = var.server_port
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = 200
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
+  }
+}
 variable "server_port" {
   description = "The port of the web server"
   type        = number
   default     = 8080
+}
+
+output "alb_dns_name" {
+  value       = aws_lb.loadbalancer.dns_name
+  description = "The domain name of the load balancer"
 }
 
 data "aws_vpc" "default" {
