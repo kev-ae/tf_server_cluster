0a31c25797da4296627fea5cbc682ad9fd483c35
kev-ae <kevin.nguyenelhs@gmail.com>
2021-11-01 01:06:26 -0400

    refactor bash script for user data and connect db

--- stage/services/webserver-cluster/main.tf
+++ stage/services/webserver-cluster/main.tf
@@ -17,11 +17,7 @@ resource "aws_launch_configuration" "instance" {
   instance_type   = "t2.micro"
   security_groups = [aws_security_group.instance.id]
 
-  user_data = <<-EOF
-    #!/bin/bash
-    echo "Hello World" > index.html
-    nohup busybox httpd -f -p ${var.server_port} &
-  EOF
+  user_data = data.template_file.user_data.rendered
 
   lifecycle {
     create_before_destroy = true
@@ -146,4 +142,14 @@ data "terraform_remote_state" "db" {
     key = "stage/data-stores/mysql/terraform.tfstate"
     region = "us-east-2"
    }
+}
+
+data "template_file" "user_data" {
+  template = file("user-data.sh")
+
+  vars = {
+    server_port = var.server_port
+    db_address = data.terraform_remote_state.db.outputs.address
+    db_port = data.terraform_remote_state.db.outputs.port
+  }
 }
